9bb38cd74391e6beada183fd0b18a8898e381d66
Nadia <n.centanaro@gmail.com>
2019-07-30 15:26:35 -0300

    detalles

--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -4,19 +4,30 @@ public abstract class Jugador {
     private Integer skillsJugador;
     private Integer pesoJugador;
     public Escoba escoba;
-    public Integer velocidadEscoba; //???????????????????????????????????
 
-    //public Integer habilidadJugador(){}
 
+    /** Constructor **/
+    public Jugador(Integer skillsJugador, Integer pesoJugador){
+        this.skillsJugador=skillsJugador;
+        this.pesoJugador=pesoJugador;
+    }
+
+    /** Punto 1.a **/
     public Integer nivelManejoDeEscoba(){
-        return getSkillsJugador() / getPesoJugador();
-    };
+        return skillsJugador/ pesoJugador;
+    }
 
+    /** Punto 1.b **/
     public Integer velocidadJugador(){
         return velocidadEscoba * nivelManejoDeEscoba();
     }
 
-    /** Getters y Setters **/
+    //public Integer velocidadEscoba; //???????????????????????????????????
+
+    //public Integer habilidadJugador(){}
+
+    
+    /** Getters **/
 
     public Integer getSkillsJugador() {
         return skillsJugador;
@@ -25,7 +36,7 @@ public abstract class Jugador {
     public Integer getPesoJugador() {
         return pesoJugador;
     }
-    
+
 
 
 
