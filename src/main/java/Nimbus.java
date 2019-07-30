557e21c897311ece3c21b408dfe9cba1c31434d3
Nadia <n.centanaro@gmail.com>
2019-07-30 18:07:55 -0300

    constructores everywhere

--- src/main/java/Nimbus.java
+++ src/main/java/Nimbus.java
@@ -1,10 +1,23 @@
 package main.java;
 
 public class Nimbus implements Escoba{
-    public Integer cantDeAniosDesdeFabricacion;
+    public Integer anioDeFabricacion;
     public Integer porcentajeSaludEscoba;
 
-    Integer velocidadEscoba(){
-        return (80 - cantDeAniosDesdeFabricacion) * porcentajeSaludEscoba;
+    public Integer anioActual(){
+        return new Date().getYear();
+    }
+
+    public Nimbus(Integer anioDeFabricacion, Integer porcentajeSaludEscoba){
+        this.anioDeFabricacion=anioDeFabricacion;
+        this.porcentajeSaludEscoba=porcentajeSaludEscoba;
+    }
+
+    public Integer cantDeAniosDesdeFabricacion(){
+        return anioActual-anioDeFabricacion;
+    }
+
+    public Integer velocidadEscoba(){
+        return (80 - cantDeAniosDesdeFabricacion()) * porcentajeSaludEscoba;
     }
 }
--- src/main/java/SaetaDeFuego.java
+++ src/main/java/SaetaDeFuego.java
@@ -1,7 +1,7 @@
 package main.java;
 
 public class SaetaDeFuego implements Escoba {
-    Integer velocidadEscoba(){
+    public Integer velocidadEscoba(){
         return 100;
     }
 
